3ccb2a2b1e773a88bded80e6f593bfa8bb068fad
githiomi <danielgithiomi@gmail.com>
2024-01-16 21:55:04 +0400

    Feat: Installed bootstrap for css

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LifecycleHooksComponent } from './components/pages/lifecycle-hooks/lifecycle-hooks.component';
 import { HomeComponent } from './components/pages/home/home.component';
+import { HeaderComponent } from './components/utilities/header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    LifecycleHooksComponent
+    LifecycleHooksComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
