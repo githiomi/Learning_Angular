33f59d53f2240d936bd8955f9a07e308d363cadd
githiomi <danielgithiomi@gmail.com>
2024-01-16 20:39:20 +0400

    Feat: Created lifecycle hooks component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PostComponent } from './components/post/post.component';
 import { PostItemComponent } from './components/post-item/post-item.component';
+import { LifecycleHooksComponent } from './components/lifecycle-hooks/lifecycle-hooks.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     PostComponent,
-    PostItemComponent
+    PostItemComponent,
+    LifecycleHooksComponent
   ],
   imports: [
     BrowserModule,
